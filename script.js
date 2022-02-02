1d2efa4bdbccf9e70a99b38604dbcba4c8f9a1e8
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-02-02 11:48:45 -0500

    Implemented add, subtract, mutliply, and divide functionality

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return parseFloat((Number(1.0 * a) + Number(1.0 * b)).toFixed(9));
+};
+
+function subtract(a, b) {
+	return parseFloat((1.0 * a - b).toFixed(9));
+};
+
+function multiply(a, b) {
+  return parseFloat((1.0 * a * b).toFixed(9));
+  /*
+    //Array parameter implementation
+    return arr.reduce((product, arrValue) => {
+    return product * arrValue;
+  }, 1)*/
+};
+
+function divide(a, b) {
+  if (Number(1.0 * b) === 0)
+    return 'Cannot divide by 0';
+
+  return parseFloat((1.0 * a / b).toFixed(9));
+};
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+};
