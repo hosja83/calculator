dd7424e3ebf80a0179ec70905afc490521865b5e
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-02-03 14:14:08 -0500

    Added number, period, clear, backspace, and sign functionality to display values to calculator

--- script.js
+++ script.js
@@ -24,9 +24,115 @@ function divide(a, b) {
 
 function operate(operator, x, y) { return operator(x, y) };
 
-module.exports = {
-  add,
-  subtract,
-  multiply,
-  divide,
-};
+function activateNumberButtonListeners() {
+  numbers.forEach(number => {number.addEventListener('click', displayNumber)});
+}
+
+function removeNumberButtonListeners() {
+  numbers.forEach(number => {number.removeEventListener('click', displayNumber)});
+}
+
+function displayNumber(event) {
+  let number = event.target.textContent; // string
+  display.textContent += number;
+
+  countCharactersDisplayed++;
+  checkCharactersDisplayedCount();
+  
+  numberCurrentlyDisplayed = display.textContent; // string
+}
+
+function activatePeriodButtonListener() { 
+  period.addEventListener('click', displayPeriod, {once : true});
+}
+
+function removePeriodButtonListener() { period.removeEventListener('click', displayPeriod); }
+
+function displayPeriod(event) {
+  let period = event.target.textContent; // string
+  display.textContent += period;
+  isPeriodUsed = true;
+
+  countCharactersDisplayed++;
+  checkCharactersDisplayedCount();
+
+  numberCurrentlyDisplayed = display.textContent; // string
+}
+
+function activatePlusMinusButtonListener() {
+  plusMinus.addEventListener('click', displayMinusSign);
+}
+
+function displayMinusSign() {
+  // Other ways of implementing.. Math.sign ... problem is -0 gets treated as 0 (sign === 0)
+  const sign = numberCurrentlyDisplayed.charAt(0);
+  if (sign === '-') {
+    // Other ways of implementing..
+    // Math.abs(), n * -1...problem is rounds off digits past long decimal numbers
+    numberCurrentlyDisplayed = numberCurrentlyDisplayed.slice(1, numberCurrentlyDisplayed.length);
+  } else if (sign !== undefined) {
+    const minus = '-';
+    numberCurrentlyDisplayed = minus + numberCurrentlyDisplayed;
+  }
+  display.textContent = numberCurrentlyDisplayed;
+}
+
+function activateBackspaceButtonListener() {
+  backspace.addEventListener('click', eraseOneCharacter);
+}
+
+function eraseOneCharacter() {
+  if (numberCurrentlyDisplayed !== undefined && numberCurrentlyDisplayed !== "") {
+    const ch = numberCurrentlyDisplayed[numberCurrentlyDisplayed.length - 1];
+    numberCurrentlyDisplayed = numberCurrentlyDisplayed.substr(0, numberCurrentlyDisplayed.length - 1);
+    if (countCharactersDisplayed === 47) {
+      countCharactersDisplayed--;
+      activateNumberButtonListeners();
+      if (ch == '.')
+        activatePeriodButtonListener();
+    } else if (ch !== '-') {
+      countCharactersDisplayed--;
+      if (ch === '.')
+        activatePeriodButtonListener();
+    }
+  }
+  display.textContent = numberCurrentlyDisplayed;
+}
+
+function activateClearButtonLister() { clear.addEventListener('click', clearDisplay); }
+
+function clearDisplay() {
+  display.textContent = "";
+  countCharactersDisplayed = 0;
+  numberCurrentlyDisplayed = "";
+
+  activateNumberButtonListeners();
+  activatePeriodButtonListener();
+}
+
+function checkCharactersDisplayedCount() {
+  if (countCharactersDisplayed >= 47 && !isPeriodUsed) {
+    removeNumberButtonListeners();
+    removePeriodButtonListener();
+  } else if (countCharactersDisplayed >= 47) {
+    removeNumberButtonListeners();
+  }
+}
+
+let countCharactersDisplayed, numberCurrentlyDisplayed, firstOperand, secondOperand, result, isPeriodUsed;
+
+countCharactersDisplayed = 0;
+isPeriodUsed = false;
+
+const numbers = document.querySelectorAll('button.number');
+const display = document.querySelector('div.display-container');
+const period = document.querySelector('button.period');
+const plusMinus = document.querySelector('button.plus-minus');
+const backspace = document.querySelector('button.backspace');
+const clear = document.querySelector('button.clear');
+
+activateNumberButtonListeners();
+activatePeriodButtonListener();
+activatePlusMinusButtonListener();
+activateBackspaceButtonListener();
+activateClearButtonLister();
